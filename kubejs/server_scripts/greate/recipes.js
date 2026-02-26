c28e5f4d572a31cf35d23b74e65c6e76737b1fef
FTL-Space-Warp <f.walhout@libero.it>
2026-02-26 11:18:56 +0100

    fixed more greate recipes

--- kubejs/server_scripts/greate/recipes.js
+++ kubejs/server_scripts/greate/recipes.js
@@ -1120,6 +1120,30 @@ function registerGreateRecipes(event) {
 		.removePreviousMaterialInfo().addMaterialInfo(true)
 
 	// #endregion
+		event.recipes.gtceu.shaped('greate:wire_coating_factory', [
+		'ABA',
+		'CDC',
+		'ABA'
+	], {
+		A: 'gtceu:watertight_casing',
+		B: '#gtceu:circuits/iv',
+		C: 'gtceu:iv_electric_pump',
+		D: 'create:spout'
+	}).addMaterialInfo().id('tfg:shaped/toms_favourite_block')
+
+	event.shaped(`2x greate:treated_wood_window`, [
+		'BAB',
+		' B '
+	], {
+		A: '#forge:glass',
+		B: 'gtceu:treated_wood_planks'
+	}).id(`tfg:greate/shaped/treated_wood_window`)
+
+	event.shapeless(`2x greate:treated_wood_window_pane`,
+	[
+		`greate:treated_wood_window`,
+		'#forge:tools/saws'
+	]).id('tfg:greate/shapeless/treated_wood_window_pane')
 
 	event.recipes.gtceu.cutter(`tfg:greate/greate:treated_wood_window_pane`)
 		.itemInputs(`3x greate:treated_wood_window`)
